69af465e72cbac2ee6e6e163048670814eeec86c
Marina8989 <marina.mill209@gmail.com>
2021-04-20 15:39:13 -0700

    random meal generator API

---
+++ main.js
@@ -0,0 +1,27 @@
+const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+const mealName = document.getElementById('mealName');
+const mealImage = document.getElementById('mealImage');
+const mealInstruction = document.getElementById('mealInstruction');
+const mealVideo = document.getElementById('mealVideo');
+
+async function getMeal() {
+    let result = await fetch(url);
+    let data = await result.json();
+
+    displayMeal(data);
+}
+
+  function displayMeal(data) {
+      console.log(data);
+      mealName.innerText = data.meals[0].strMeal;
+      mealImage.src = data.meals[0].strMealThumb;
+
+  }
+
+
+
+
+
+
+getMeal();
